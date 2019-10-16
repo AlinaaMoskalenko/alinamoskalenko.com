d1bc84cc64a408381ad48a3b94493f1de1081256
Alina Moskalenko <alina.kravchenko@ukr.net>
2019-10-16 22:28:33 +0300

    Created first-meet content (desktop, laptop devices)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 import './index.scss';
 
-import Background from './scripts/background';
+import { renderSocialLinks } from './scripts/renderSocialLinks';
+import { renderContactMe } from './scripts/renderContactMe';
 
 import main_illustration from './common/assets/backgrounds/illustration_1.svg';
 import texture from './common/assets/backgrounds/texture_1.svg';
 import bg_1 from './common/assets/backgrounds/back_1.svg';
 
+import { renderBackground } from './scripts/renderBackground';
+
+// social links
+const socialLinks = renderSocialLinks();
+
+{ // first meet block
+  const root = document.querySelector('.first-meet__content');
+  const main = root.querySelector('.main');
+
+  main.appendChild(socialLinks);
+  root.appendChild(renderContactMe());
+}
+
 { // background 1
-  const root = document.querySelector('.content-block-1');
+  const root = document.querySelector('.grid-container-1');
   const backgroundElements = [
-    { img: bg_1, classname: 'content-block-1__background', alt: 'Background' },
-    { img: main_illustration, classname: 'content-block-1__illustration', alt: 'Illustration' },
-    { img: texture, classname: 'content-block-1__texture', alt: 'Texture' }
+    { url: bg_1, classname: 'grid-item__background', alt: 'Background' },
+    { url: main_illustration, classname: 'grid-item__illustration', alt: 'Illustration' },
+    { url: texture, classname: 'grid-item__texture', alt: 'Texture' }
   ];
 
   createBackground(root, backgroundElements);
 }
 
 function createBackground(root, elements) {
-  new Background(root, elements);
+  renderBackground(root, elements);
 }
--- src/scripts/renderContactMe.js
+++ src/scripts/renderContactMe.js
@@ -1,22 +1,23 @@
-import primary from '../common/assets/ic-contact/ic-contact.svg';
-import white from '../common/assets/ic-contact/ic-contact-white.svg';
-
-import { renderImage } from './renderImage';
-
-export const renderContactMe = (mode = 'primary', href = '#contact-me') => {
-  const icon = mode === 'white' ? white : primary;
-  const contactIcon = { url: icon, alt: 'letter' };
-
+export const renderContactMe = (href = '#contact-me') => {
+  const icon = 
+  ` <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <path class="ic-contact" fill-rule="nonzero" d="M19.995 1.004l-1.603 17a1.128 1.128 0 0 1-.517.852 1.073 1.073 0 0 1-.974.09l-5.314-2.107L8.609 19.7a1.038 1.038 0 0 1-.726.299 1.06 1.06 0 0 1-.765-.32 1.114 1.114 0 0 1-.315-.783V16.86l7.172-9.182a.487.487 0 0 0 .056-.505.465.465 0 0 0-.42-.271.45.45 0 0 0-.297.12l-8.279 7.214-4.362-1.732a1.087 1.087 0 0 1-.67-.943c-.027-.421.186-.82.547-1.024L18.67.12a.872.872 0 0 1 .927.03c.279.187.433.519.398.859v-.004z"/>
+    </svg> `;
+    
   const rootElement = document.createElement('a');
   const text = document.createElement('p');
+  const iconElement = document.createElement('div');
 
   rootElement.setAttribute('href', href);
   rootElement.classList.add('contact-me-link');
 
   text.textContent = 'Contact me';
 
+  iconElement.classList.add('contact-me-link__icon');
+  iconElement.innerHTML = icon;
+
   rootElement.appendChild(text);
-  renderImage(rootElement, contactIcon);
+  rootElement.appendChild(iconElement);
 
   return rootElement;
 };
